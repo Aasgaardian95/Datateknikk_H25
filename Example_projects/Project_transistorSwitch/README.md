3a8e2316b54a05f49c4bddf3e6d308078d6af67c
Marius A. J. Aasgaard <aasgaardmarius@gmail.com>
2025-11-18 15:12:18 +0100

    Lagt et miniprosjekt. Transistor m/LED

---
+++ Example_projects/Project_transistorSwitch/README.md
@@ -0,0 +1,11 @@
+## Enkel transistor m/LED
+...
+
+## Oppkobling
+- Transistor: 
+    - collector -> GND
+    - base -> 1k ohm -> digital 8
+    - emitter -> LED cathode
+- LED: 
+    - anode -> 1k ohm -> 5V
+    - cathode -> emitter
---
+++ Example_projects/Project_transistorSwitch/src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+const int transistor = 8;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode (transistor,  OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite (transistor, HIGH);
+  Serial.println("Transistor ON");
+  delay(250);
+  digitalWrite (transistor, LOW);
+  Serial.println("Transistor OFF");
+  delay(250);
+}
