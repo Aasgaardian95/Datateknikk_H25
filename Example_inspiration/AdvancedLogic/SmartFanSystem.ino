9be1c50f790c0295526c0987aff2317fa4f112dc
Marius A. J. Aasgaard <aasgaardmarius@gmail.com>
2025-11-21 00:26:39 +0100

    Fiks fiks igjen?

--- Example_inspiration/AdvancedLogic/SmartFanSystem.ino
+++ Example_inspiration/AdvancedLogic/SmartFanSystem.ino
@@ -8,12 +8,20 @@ Logikk:
     - Vintermodus: motsatt retning
 - Motorhastighet justeres basert på temperatur og luftkvalitet
 - Manuell overstyring via knapp gir full hastighet
-- Serial Monitor viser temperatur, fuktighet, luftkvalitet, motorhastighet og retning
+- OLED viser sanntidsdata: Temp, Hum, TVOC, eCO2, motorhastighet og retning
+- Serial Monitor brukes for debugging
 */
 
 #include <Wire.h>                    // I2C-kommunikasjon
 #include <SparkFunBME280.h>          // SparkFun BME280 bibliotek
 #include <SparkFun_ENS160.h>         // SparkFun ENS160 bibliotek
+#include <Adafruit_GFX.h>            // OLED grafikkbibliotek
+#include <Adafruit_SSD1306.h>        // OLED displaybibliotek
+
+#define SCREEN_WIDTH 128             // OLED bredde i piksler
+#define SCREEN_HEIGHT 64             // OLED høyde i piksler
+#define OLED_RESET    -1             // Ingen reset-pin på OLED
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); // OLED objekt
 
 #define MOTOR_PWM 9                  // PWM-pin til motor/vifte
 #define MOTOR_DIR 8                  // Retningspinne til motor
@@ -23,63 +31,79 @@ BME280 myBME;                        // Oppretter BME280 objekt
 ENS160 myENS;                         // Oppretter ENS160 objekt
 
 bool summerMode = true;               // Modus: sommer/vinter
-int motorSpeed = 0;                    // Motorhastighet (0-255)
+int motorSpeed = 0;                    // Motorhastighet 0-255
 
 void setup() {
-  Serial.begin(9600);                 // Start Serial Monitor
+  Serial.begin(9600);                 // Start Serial Monitor for debugging
 
   // --- Initialiser BME280 ---
-  if (!myBME.begin()) {               // Sjekker om sensor svarer
+  if (!myBME.begin()) {               // Sjekk om sensor finnes
     Serial.println("BME280 ikke funnet!");
-    while (1);                        // Stopp koden hvis ikke funnet
+    while (1);                        // Stopp her hvis ikke funnet
   }
 
   // --- Initialiser ENS160 ---
-  if (myENS.begin() != 0) {           // Sjekker om sensor svarer
+  if (myENS.begin() != 0) {           // Sjekk om sensor finnes
     Serial.println("ENS160 ikke funnet!");
     while (1);
   }
-  myENS.iaqInit();                     // Start IAQ-måling
+  myENS.iaqInit();                     // Initialiser ENS160 IAQ
+
+  // --- Initialiser OLED ---
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Sjekk om OLED finnes
+    Serial.println("OLED ikke funnet!");
+    while(1);
+  }
+  display.clearDisplay();              // Tøm OLED
+  display.setTextSize(1);             // Sett tekststørrelse
+  display.setTextColor(SSD1306_WHITE);// Sett tekstfarge
+  display.setCursor(0,0);             // Start øverst til venstre
+  display.println("Smart Vifte");     
+  display.display();                   // Vis tekst
 
   // --- Aktuatorpinner ---
-  pinMode(MOTOR_PWM, OUTPUT);         // PWM til motorhastighet
-  pinMode(MOTOR_DIR, OUTPUT);         // Digital pin for retning
+  pinMode(MOTOR_PWM, OUTPUT);         // PWM til motor
+  pinMode(MOTOR_DIR, OUTPUT);         // Digital retning
   pinMode(OVERRIDE_BUTTON, INPUT_PULLUP); // Knapp med pullup
 }
 
 void loop() {
-  // --- Les temperatur og fuktighet ---
-  float temp = myBME.readTempC();           // Temperatur i °C
-  float hum = myBME.readFloatHumidity();    // Relativ fuktighet %
+  float temp = myBME.readTempC();           // Les temperatur °C
+  float hum = myBME.readFloatHumidity();    // Les relativ fuktighet %
 
-  // --- Kompenser ENS160 med temp og fuktighet ---
-  myENS.setEnvironmentalData((int)temp, (int)hum);
-  myENS.iaqMeasure();                        // Start måling
+  myENS.setEnvironmentalData((int)temp, (int)hum); // Mate ENS160 med temp/hum
+  myENS.iaqMeasure();                        // Start IAQ-måling
 
-  // --- Les luftkvalitet ---
-  uint16_t tvoc = myENS.getTVOC();          // Total volatile organic compounds
-  uint16_t eco2 = myENS.getCO2();           // Estimert CO2
+  uint16_t tvoc = myENS.getTVOC();          // Les TVOC (ppb)
+  uint16_t eco2 = myENS.getCO2();           // Les eCO2 (ppm)
 
-  // --- Les knapp for manuell overstyring ---
-  bool override = digitalRead(OVERRIDE_BUTTON) == LOW; // Knapp trykket = LOW
+  bool override = digitalRead(OVERRIDE_BUTTON) == LOW; // Knapp trykket?
 
-  // --- Sett motorretning basert på modus ---
-  if (summerMode) digitalWrite(MOTOR_DIR, HIGH);  // Sommer: én retning
-  else digitalWrite(MOTOR_DIR, LOW);              // Vinter: motsatt retning
+  if (summerMode) digitalWrite(MOTOR_DIR, HIGH); // Sommermodus: én retning
+  else digitalWrite(MOTOR_DIR, LOW);            // Vintermodus: motsatt retning
 
-  // --- Beregn motorhastighet basert på temperatur og luftkvalitet ---
-  if (!override) {                               // Kun hvis ikke overstyrt
-    if (temp < 20 && tvoc < 250) motorSpeed = 50;     // Lav temp og god luft = lav hastighet
-    else if (temp < 25 && tvoc < 300) motorSpeed = 120; // Moderat temp/lufthastighet
-    else motorSpeed = 255;                        // Høy temp/dårlig luft = full hastighet
+  if (!override) {                              // Hvis ikke overstyrt
+    if (temp < 20 && tvoc < 250) motorSpeed = 50;      // Lav temp + god luft = lav hastighet
+    else if (temp < 25 && tvoc < 300) motorSpeed = 120; // Moderat
+    else motorSpeed = 255;                         // Høy temp / dårlig luft
   } else {
-    motorSpeed = 255;                             // Overstyring = full hastighet
+    motorSpeed = 255;                              // Overstyring = full hastighet
   }
 
-  // --- Sett PWM til motor ---
-  analogWrite(MOTOR_PWM, motorSpeed);            // Motorhastighet 0-255
+  analogWrite(MOTOR_PWM, motorSpeed);             // Sett motor PWM
 
-  // --- Serial Monitor for debugging ---
+  // --- Oppdater OLED ---
+  display.clearDisplay();                        // Tøm skjermen
+  display.setCursor(0,0);                        // Start øverst
+  display.print("Temp: "); display.print(temp); display.println(" C");
+  display.print("Hum: "); display.print(hum); display.println(" %");
+  display.print("TVOC: "); display.print(tvoc); display.println(" ppb");
+  display.print("eCO2: "); display.print(eco2); display.println(" ppm");
+  display.print("Motor: "); display.print(motorSpeed); display.print("/255");
+  display.println(summerMode ? " Sommer" : " Vinter");
+  display.display();                             // Vis OLED
+
+  // --- Serial Monitor ---
   Serial.print("Temp: "); Serial.print(temp); Serial.print(" C, ");
   Serial.print("Hum: "); Serial.print(hum); Serial.print(" %, ");
   Serial.print("TVOC: "); Serial.print(tvoc); Serial.print(" ppb, ");
@@ -87,5 +111,5 @@ void loop() {
   Serial.print("Motor: "); Serial.print(motorSpeed); Serial.print("/255, ");
   Serial.print("Retning: "); Serial.println(summerMode ? "Sommer" : "Vinter");
 
-  delay(500);  // Oppdatering hvert 0,5 sekund
+  delay(500);                                   // Oppdater hvert 0,5 sek
 }
